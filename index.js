bf71976dd094bb18289549986e94fab2253201a9
WillTDA <62032600+WillTDA@users.noreply.github.com>
2021-04-02 23:27:20 +0100

    bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const prompter = require("discordjs-prompter");
 const { Aki } = require("aki-api");
+const games = new Set();
 
 module.exports = {
 
@@ -15,7 +16,7 @@ module.exports = {
     * const PREFIX = "!";
     * 
     * client.on("message", async message => {
-    *     if(message.content.startsWith(`${PREFIX}akinator`)) {
+    *     if(message.content.startsWith(`${PREFIX}akiantor`)) {
     *         akinator.play(message)
     *     }
     * });
@@ -29,34 +30,48 @@ module.exports = {
 
         let notFinished = true;
 
-        let akiMessage = await message.channel.send(
-            new MessageEmbed()
-                .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
-                .setTitle(`Question ${aki.currentStep + 1}`)
-                .setDescription(`**${aki.question}**`)
-                .addField("Please Type...", "**Y** or **Yes**\n**N** or **No**\n**IDK** or **Don't Know**\n**P** or **Probably**\n**PN** or **Probably Not**")
-                .setColor("RANDOM")
-        )
+        let akiEmbed = new Discord.MessageEmbed()
+            .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
+            .setTitle(`Question ${aki.currentStep + 1}`)
+            .setDescription(`**${aki.question}**`)
+            .addField("Please Type...", "**Y** or **Yes**\n**N** or **No**\n**IDK** or **Don't Know**\n**P** or **Probably**\n**PN** or **Probably Not**")
+            .setColor("RANDOM")
+
+        let akiMessage = await message.channel.send(akiEmbed)
 
         while (notFinished) {
             if (aki.currentStep + 1 >= 2) akiMessage.edit(
-                new MessageEmbed()
+                new Discord.MessageEmbed()
                     .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
                     .setTitle(`Question ${aki.currentStep + 1}`)
                     .setDescription(`**${aki.question}**`)
                     .addField("Please Type...", "**Y** or **Yes**\n**N** or **No**\n**IDK** or **Don't Know**\n**P** or **Probably**\n**PN** or **Probably Not**")
                     .setColor("RANDOM")
             )
-            prompter.message(message.channel, {
-                question: akiMessage,
-                userId: message.author.id,
-                max: 1,
-                timeout: 60000,
+
+            const filter = x => {
+                return (x.author.id === message.author.id && ([
+                    "y",
+                    "yes",
+                    "n",
+                    "no",
+                    "idk",
+                    "dont know",
+                    "don't know",
+                    "p",
+                    "probably",
+                    "pn",
+                    "probably not"
+                ].includes(x.content.toLowerCase())));
+            }
+
+            await message.channel.awaitMessages(filter, {
+                max: 1, time: 128000
             })
                 .then(async responses => {
                     if (!responses.size) {
                         return message.channel.send(
-                            new MessageEmbed()
+                            new Discord.MessageEmbed()
                                 .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
                                 .setTitle(`Game Ended`)
                                 .setDescription(`**${message.author.username}, your Game has Ended due to 1 Minute of Inactivity.**`)
@@ -78,12 +93,13 @@ module.exports = {
                         "probably not": 4,
                     }
 
-                    await aki.step(answers[answer])
+                    await aki.step(answers[answer]);
+                    await responses.first().delete();
 
                     if (aki.progress >= 95 || aki.currentStep >= 78) {
                         await aki.win();
                         await akiMessage.edit(
-                            new MessageEmbed()
+                            new Discord.MessageEmbed()
                                 .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
                                 .setTitle(`I'm ${Math.round(aki.progress)}% Sure your Character is...`)
                                 .addField("Your Character", `**${aki.answers[0].name}**`)
