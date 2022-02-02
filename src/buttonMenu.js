b81649425e39b7afd8c27a3d25defce97bd0c2bd
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-02-02 23:53:28 +0800

    v14 compatibility
    - added compatibility for discord.js@dev (v14)

--- src/buttonMenu.js
+++ src/buttonMenu.js
@@ -1,39 +1,48 @@
 const Discord = require("discord.js");
-const { MessageButton, MessageActionRow } = require("discord.js");
 
 /**
  * @param {Discord.Client} client The Discord Client.
  * @param {any} input The Message Sent by the User.
  * @param {Discord.Message} botMessage The Message for the Bot to Send, also the message which will contain the buttons (Max. 8). MUST BE AN EMBED!
- * @param {MessageButton[]} buttons An Array of Buttons.
+ * @param {Discord.MessageButton[]} buttons An Array of Buttons.
  * @param {Number} time Time in Milliseconds the Menu should last for.
  */
 
 module.exports = async function (client, input, botMessage, buttons, time) {
     //check all our params exist
+    let errorButton = false
     if (!client) return console.log("Button Menu Error: No Client Provided!")
     if (!input) return console.log("Button Menu Error: No Message Provided!")
     if (!botMessage) return console.log("Button Menu Error: No Bot Message Provided!")
     if (!buttons) return console.log("Button Menu Error: No Buttons Provided!")
     if (!time) return console.log("Button Menu Error: No Time Provided!")
+    if (typeof buttons !== "object") return console.log("Button Menu Error: Provided Button is not an array!")
     
-    let buttonRow = new MessageActionRow()
-    let buttonRow2 = new MessageActionRow()
-    let buttonRow3 = new MessageActionRow()
+    
+    let buttonRow = { type: 1, components: [] }
+    let buttonRow2 = { type: 1, components: [] }
+    let buttonRow3 = { type: 1, components: [] }
     let buttonRows = []
+    
+    
 
     for (let i = 0; i < buttons.length; i++) {
-        if (i < 3) {
-            buttonRow.addComponents(buttons[i]);
+        if ((!buttons[i]?.customId && !buttons[i].custom_id) || !buttons[i]?.label || !buttons[i]?.style) {
+            errorButton = true
+            i = 9999
+        } else if (i < 3) {
+            buttonRow.push(buttons[i]);
         }
         else if (i < 5) {
-            buttonRow2.addComponents(buttons[i])
+            buttonRow2.push(buttons[i])
         }
         else {
-            buttonRow3.addComponents(buttons[i])
+            buttonRow3.push(buttons[i])
         }
 
     }
+    
+    if (errorButton === true) return console.log("Button Menu Error: Provided buttons were not a valid button object!")
 
     buttonRows.push(buttonRow)
     if (buttons.length >= 5) buttonRows.push(buttonRow2)
